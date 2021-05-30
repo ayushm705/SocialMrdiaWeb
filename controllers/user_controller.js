7d9e9ed25cfd16779ccd57de2a181455a6076f61
AYUSH MISHRA <ayushm705@gmail.com>
2021-05-30 20:21:34 +0530

    Sign up and Sign in page created

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -9,3 +9,28 @@ module.exports.profile=function(req,res)
         title:"User Profile"
     });
 } 
+
+
+//render the sign up page
+module.exports.signUp=function(req,res)
+{
+    return res.render('user_sign_up',{
+        title:"Codeial | Sign Up" 
+    })
+}
+
+//render the sign in page
+module.exports.signIn=function(req,res){
+    return res.render('user_sign_in',{
+        title:"Codeial |Sign In"
+    })
+}
+//get the sign up date
+module.exports.create=function(req,res){
+    //Todo Later
+
+}
+//sign in and create session for the user
+module.exports.createSession=function(req,res){
+    //Todo Later
+}
--- routes/users.js
+++ routes/users.js
@@ -4,3 +4,6 @@ const router=express.Router();
 const usersControllers=require('../controllers/user_controller');
 router.get('/profile',usersControllers.profile);
 module.exports=router;
+
+router.get('/sign-up',usersControllers.signUp);
+router.get('/sign-in',usersControllers.signIn);
